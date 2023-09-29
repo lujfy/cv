43fb4f082a3dd14c10acaed5cae4fbada6d192fe
lujfy <lujfy50@gmail.com>
2023-09-29 16:30:43 +0700

    project odin cv application final

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header/Header'
+import './App.css'
+import Body from './components/Body/Body'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  )
+}
+
+export default App
